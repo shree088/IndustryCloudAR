cf529c2f2eb81083b9a0750ea0a4f249137e3e52
srikanthgundala <38369410+srikanthgundala@users.noreply.github.com>
2023-08-25 12:38:22 +0530

    First commit
    First commit

---
+++ SampleXamarin.Android/AnchorVisual.cs
@@ -0,0 +1,185 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Android.Content;
+using Google.AR.Core;
+using Google.AR.Sceneform;
+using Google.AR.Sceneform.Math;
+using Google.AR.Sceneform.Rendering;
+using Google.AR.Sceneform.UX;
+using Java.Util.Concurrent;
+using Microsoft.Azure.SpatialAnchors;
+using System;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+
+namespace SampleXamarin
+{
+    internal class AnchorVisual
+    {
+        public enum NamedShape
+        {
+            Sphere,
+            Cube,
+            Cylinder,
+        }
+
+        private TransformableNode transformableNode;
+        private NamedShape shape = NamedShape.Sphere;
+        private Material material;
+        private ArFragment ArFragment;
+        private AzureSpatialAnchorsCoarseRelocActivity activity;
+        private static Dictionary<int, CompletableFuture> solidColorMaterialCache = new Dictionary<int, CompletableFuture>();
+
+        public AnchorVisual(ArFragment arFragment, Anchor localAnchor)
+        {
+            AnchorNode = new AnchorNode(localAnchor); 
+            this.ArFragment = arFragment;
+
+             transformableNode = new TransformableNode(arFragment.TransformationSystem);
+            transformableNode.ScaleController.Enabled = false;
+            transformableNode.TranslationController.Enabled = false;
+            transformableNode.RotationController.Enabled = false;
+            transformableNode.SetParent(AnchorNode);
+        }
+        public AnchorVisual(ArFragment arFragment, CloudSpatialAnchor cloudAnchor, AzureSpatialAnchorsCoarseRelocActivity activity)
+            : this(arFragment, cloudAnchor.LocalAnchor)
+        {
+            CloudAnchor = cloudAnchor;
+            this.ArFragment = arFragment;
+            this.activity = activity;
+        }
+
+        public AnchorVisual(ArFragment arFragment, CloudSpatialAnchor cloudAnchor)
+           : this(arFragment, cloudAnchor.LocalAnchor)
+        {
+            CloudAnchor = cloudAnchor;
+            this.ArFragment = arFragment;
+        }
+
+        public AnchorNode AnchorNode { get; }
+
+        public CloudSpatialAnchor CloudAnchor { get; set; }
+
+        public Anchor LocalAnchor => this.AnchorNode.Anchor;
+
+        public NamedShape Shape
+        {
+            get { return shape; }
+            set
+            {
+                if (shape != value)
+                {
+                    shape = value;
+                    MainThread.BeginInvokeOnMainThread(RecreateRenderableOnMainThread);
+                }
+            }
+        }
+
+        public bool IsMovable
+        {
+            set
+            {
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    transformableNode.TranslationController.Enabled = value;
+                    transformableNode.RotationController.Enabled = value;
+                });
+            }
+        }
+
+        public void AddToScene(ArFragment arFragment)
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                RecreateRenderableOnMainThread();
+                AnchorNode.SetParent(arFragment.ArSceneView.Scene);
+            });
+        }
+
+        public void SetColor(Context context, int rgb)
+        {
+            lock (this)
+            {
+                if (!solidColorMaterialCache.ContainsKey(rgb))
+                {
+                    solidColorMaterialCache[rgb] = MaterialFactory.MakeOpaqueWithColor(context, new Color(rgb));
+                }
+                CompletableFuture loadMaterial = solidColorMaterialCache[rgb];
+                loadMaterial.ThenAccept(new FutureResultConsumer<Material>(SetMaterial));
+            }
+        }
+
+        public void SetMaterial(Material material)
+        {
+            if (this.material != material)
+            {
+                this.material = material;
+                MainThread.BeginInvokeOnMainThread(RecreateRenderableOnMainThread);
+            }
+        }
+
+        public void Destroy()
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                AnchorNode.Renderable = null;
+                AnchorNode.SetParent(null);
+                Anchor localAnchor = AnchorNode.Anchor;
+                if (localAnchor != null)
+                {
+                    AnchorNode.Anchor = null;
+                    localAnchor.Detach();
+                }
+            });
+        }
+
+   
+        private void RecreateRenderableOnMainThread()
+        {
+            if (material != null)
+            {
+                Renderable renderable;
+                switch (shape)
+                {
+                    case NamedShape.Sphere:
+                        renderable = ShapeFactory.MakeSphere(
+                                0.1f,
+                                new Vector3(0.0f, 0.1f, 0.0f),
+                                material);
+                        transformableNode.Renderable = renderable;
+                        break;
+                    case NamedShape.Cube:
+                        renderable = ShapeFactory.MakeCube(
+                                new Vector3(0.161f, 0.161f, 0.161f),
+                                new Vector3(0.0f, 0.0805f, 0.0f),
+                                material);
+                        transformableNode.Renderable = renderable;
+                        break;
+                    case NamedShape.Cylinder:
+                        //renderable = ShapeFactory.MakeCylinder(
+                        //        0.0874f,
+                        //        0.175f,
+                        //        new Vector3(0.0f, 0.0875f, 0.0f),
+                        //        material);
+                        var builder = ModelRenderable.InvokeBuilder();
+                        builder.SetSource(this.ArFragment.Context, Resource.Raw.fox_face);
+                        builder.Build().ThenAccept(new FutureResultConsumer<ModelRenderable>(FinishLoading));
+                        //var javaClass = Java.Lang.Class.FromType(builder.GetType());
+                        //var methods = javaClass.GetMethods();
+                        //var method = methods[11]; // setSource method
+                        //method.Invoke(builder, this.activity, Resource.Raw.fox_face);
+                        //builder.Build(FinishLoading);
+                        break;
+                    default:
+                        throw new InvalidOperationException("Invalid shape");
+                }
+                
+            }
+        }
+        private void FinishLoading(ModelRenderable model)
+        {
+            transformableNode.Renderable = model;
+        }
+    }
+}
---
+++ SampleXamarin.Common/AccountDetails.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+namespace SampleXamarin
+{
+    public static class AccountDetails
+    {
+        /// <summary>
+        /// The Azure Spatial Anchors account identifier.
+        /// </summary>
+        /// <remarks>
+        /// Set this to your account id found in the Azure Portal.
+        /// </remarks>
+        public const string SpatialAnchorsAccountId = "8f28ecdc-790c-41b4-9bab-ef5be9b31050";
+
+        /// <summary>
+        /// The Azure Spatial Anchors account key.
+        /// Set this to your account id found in the Azure Portal.
+        /// </summary>
+        /// <remarks>
+        /// Set this to your account key found in the Azure Portal.
+        /// </remarks>
+        public const string SpatialAnchorsAccountKey = "yPqcq/vWarKfyw28bwjrNTwdM7BOzDxy8OFd4BDHffA=";
+
+        /// <summary>
+        /// The Azure Spatial Anchors account domain.
+        /// Set this to your account domain found in the Azure Portal.
+        /// </summary>
+        /// <remarks>
+        /// Set this to your account domain found in the Azure Portal.
+        /// </remarks>
+        public const string SpatialAnchorsAccountDomain = "eastus.mixedreality.azure.com";
+
+        /// <summary>
+        /// The full URL endpoint of the anchor sharing service.
+        /// </summary>
+        /// <remarks>
+        /// Set this to your URL created when publishing your anchor sharing service in the Sharing sample.
+        /// It should end in '/api/anchors'.
+        /// </remarks>
+        public const string AnchorSharingServiceUrl = "Set me";
+    }
+}
